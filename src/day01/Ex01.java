c48991c93ffe1e1b388570830b68d61237a04b92
nayeon <koony0831@naver.com>
2023-04-10 14:08:14 +0900

    1일차

---
+++ src/day01/Ex01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		//System.out.println("배불렁");
+		//System.out.println("집가고싶엉");
+		System.out.print("슬슬 졸린데?");
+		System.out.println("양치도구 챙기기");
+	}
+}
